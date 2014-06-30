409762bd9e1c305c9ce5608e4504f6d4341a9191
Michael Scovetta <michael.scovetta@gmail.com>
2014-06-29 22:56:09 -0700

    Moved curses out of game play into gfx module.

--- core/game.py
+++ core/game.py
@@ -2,7 +2,8 @@ import curses
 import traceback
 import sys
 
-stdscr = None
+from core import gfx
+
 
 # A Game rpresents a single instance of a game, including its maps,
 # data, and everything else.
@@ -14,7 +15,7 @@ class Game(object):
         running = True
         x, y = 5, 5
         while running:
-            c = stdscr.getch()
+            c = gfx.scr().getch()
 
             if c == curses.KEY_UP:      y -= 1
             elif c == curses.KEY_DOWN:  y += 1
@@ -22,38 +23,19 @@ class Game(object):
             elif c == curses.KEY_RIGHT: x += 1
             elif c == ord('q'): running = False
             if c != -1:
-                stdscr.clear()
-                stdscr.addch(y, x, "@")
+                gfx.scr().clear()
+                gfx.scr().addch(y, x, "@")
 
     # Starts an interactive session between our player and our game object.
     def play(self):
-        global stdscr
-
-        stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        curses.curs_set(0)
-        stdscr.keypad(1)
-        stdscr.timeout(0)
-
+        gfx.start()
 
         try:
             self.step()
         except:
-            curses.nocbreak()
-            stdscr.timeout(-1)
-            stdscr.keypad(0)
-            curses.echo()
-            curses.endwin()
-
+            gfx.stop()
             print(traceback.format_exc())
-
             sys.exit(-1)
 
-        curses.nocbreak()
-        curses.curs_set(1)
-        stdscr.timeout(-1)
-        stdscr.keypad(0)
-        curses.echo()
-        curses.endwin()
+        gfx.stop()
 
